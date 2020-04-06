5f97b7552dea9e8f06b91ffa27421773ac3e70c2
Sri Balaji <55683994+Balaji751@users.noreply.github.com>
2020-04-06 19:54:26 +0530

    Add files via upload

---
+++ countsquares.py
@@ -0,0 +1,20 @@
+for i in range(int(input())):
+    a=int(input())
+    count=0
+    k=1
+    while(k*k<a):
+        count+=1
+        k+=1
+    print(count)
+    
+
+
+
+Input :
+2
+9
+3
+
+Output :
+2
+1
